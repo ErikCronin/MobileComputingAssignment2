d26de105dbd74be3f4c52b4a6ea4fc48b0ea5216
ErikCronin <erik.cronin@my.jcu.edu.au>
2019-05-20 22:54:53 +1000

    Changed the way the program reads in the cards to make it easier to add. still must get the cards to add together and show up on the top of the screen.

--- app/src/main/java/com/derk/mobilecomputingassignment2/GameActivity.java
+++ app/src/main/java/com/derk/mobilecomputingassignment2/GameActivity.java
@@ -21,71 +21,86 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        Random rand = new Random();
-
         AssetManager manager = getAssets();
-        try{
-            String[] number = manager.list("cards");
-            System.out.println(Arrays.toString(number));
+        String[] number = new String[0];
+        try {
+            number = manager.list("cards");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Random rand = new Random();
+        int cardOne = rand.nextInt(9+1);
+        int cardTwo = rand.nextInt(9+1);
+        int cardThree = rand.nextInt(9+1);
+        int cardFour = rand.nextInt(9+1);
+        int cardFive = rand.nextInt(9+1);
+        int cardSix = rand.nextInt(9+1);
+        int cardSeven = rand.nextInt(9+1);
+        int cardEight = rand.nextInt(9+1);
+        int cardNine = rand.nextInt(9+1);
+        int cardTen = rand.nextInt(9+1);
+        int cardEleven = rand.nextInt(9+1);
+        int cardTwelve = rand.nextInt(9+1);
 
-            ImageView imageview = findViewById(R.id.imageView);
+        try{
+            ImageView imageview = findViewById(R.id.imageButton);
             assert number != null;
-            InputStream stream = manager.open("cards/" + number[rand.nextInt(10+2)]);
+            InputStream stream = manager.open("cards/" + number[cardOne]);
             Bitmap bitmap = BitmapFactory.decodeStream(stream);
             imageview.setImageBitmap(bitmap);
 
-            ImageView imageview2 = findViewById(R.id.imageView2);
-            InputStream stream2 = manager.open("cards/" + number[rand.nextInt(10+2)]);
+            ImageView imageview2 = findViewById(R.id.imageButton2);
+            InputStream stream2 = manager.open("cards/" + number[cardTwo]);
             Bitmap bitmap2 = BitmapFactory.decodeStream(stream2);
             imageview2.setImageBitmap(bitmap2);
 
-            ImageView imageview3 = findViewById(R.id.imageView3);
-            InputStream stream3 = manager.open("cards/" + number[rand.nextInt(10+2)]);
+            ImageView imageview3 = findViewById(R.id.imageButton3);
+            InputStream stream3 = manager.open("cards/" + number[cardThree]);
             Bitmap bitmap3 = BitmapFactory.decodeStream(stream3);
             imageview3.setImageBitmap(bitmap3);
 
-            ImageView imageview4 = findViewById(R.id.imageView4);
-            InputStream stream4 = manager.open("cards/" + number[rand.nextInt(10+2)]);
+            ImageView imageview4 = findViewById(R.id.imageButton4);
+            InputStream stream4 = manager.open("cards/" + number[cardFour]);
             Bitmap bitmap4 = BitmapFactory.decodeStream(stream4);
             imageview4.setImageBitmap(bitmap4);
 
-            ImageView imageview5 = findViewById(R.id.imageView5);
-            InputStream stream5 = manager.open("cards/" + number[rand.nextInt(10+2)]);
+            ImageView imageview5 = findViewById(R.id.imageButton5);
+            InputStream stream5 = manager.open("cards/" + number[cardFive]);
             Bitmap bitmap5 = BitmapFactory.decodeStream(stream5);
             imageview5.setImageBitmap(bitmap5);
 
-            ImageView imageview6 = findViewById(R.id.imageView6);
-            InputStream stream6 = manager.open("cards/" + number[rand.nextInt(10+2)]);
+            ImageView imageview6 = findViewById(R.id.imageButton6);
+            InputStream stream6 = manager.open("cards/" + number[cardSix]);
             Bitmap bitmap6 = BitmapFactory.decodeStream(stream6);
             imageview6.setImageBitmap(bitmap6);
 
-            ImageView imageview7 = findViewById(R.id.imageView7);
-            InputStream stream7 = manager.open("cards/" + number[rand.nextInt(10+2)]);
+            ImageView imageview7 = findViewById(R.id.imageButton7);
+            InputStream stream7 = manager.open("cards/" + number[cardSeven]);
             Bitmap bitmap7 = BitmapFactory.decodeStream(stream7);
             imageview7.setImageBitmap(bitmap7);
 
-            ImageView imageview8 = findViewById(R.id.imageView8);
-            InputStream stream8 = manager.open("cards/" + number[rand.nextInt(10+2)]);
+            ImageView imageview8 = findViewById(R.id.imageButton8);
+            InputStream stream8 = manager.open("cards/" + number[cardEight]);
             Bitmap bitmap8 = BitmapFactory.decodeStream(stream8);
             imageview8.setImageBitmap(bitmap8);
 
-            ImageView imageview9 = findViewById(R.id.imageView9);
-            InputStream stream9 = manager.open("cards/" + number[rand.nextInt(10+2)]);
+            ImageView imageview9 = findViewById(R.id.imageButton9);
+            InputStream stream9 = manager.open("cards/" + number[cardNine]);
             Bitmap bitmap9 = BitmapFactory.decodeStream(stream9);
             imageview9.setImageBitmap(bitmap9);
 
-            ImageView imageview10 = findViewById(R.id.imageView10);
-            InputStream stream10 = manager.open("cards/" + number[rand.nextInt(10+2)]);
+            ImageView imageview10 = findViewById(R.id.imageButton10);
+            InputStream stream10 = manager.open("cards/" + number[cardTen]);
             Bitmap bitmap10 = BitmapFactory.decodeStream(stream10);
             imageview10.setImageBitmap(bitmap10);
 
-            ImageView imageview11 = findViewById(R.id.imageView11);
-            InputStream stream11 = manager.open("cards/" + number[rand.nextInt(10+2)]);
+            ImageView imageview11 = findViewById(R.id.imageButton11);
+            InputStream stream11 = manager.open("cards/" + number[cardEleven]);
             Bitmap bitmap11 = BitmapFactory.decodeStream(stream11);
             imageview11.setImageBitmap(bitmap11);
 
-            ImageView imageview12 = findViewById(R.id.imageView12);
-            InputStream stream12 = manager.open("cards/" + number[rand.nextInt(10+2)]);
+            ImageView imageview12 = findViewById(R.id.imageButton12);
+            InputStream stream12 = manager.open("cards/" + number[cardTwelve]);
             Bitmap bitmap12 = BitmapFactory.decodeStream(stream12);
             imageview12.setImageBitmap(bitmap12);
         } catch (IOException e) {
@@ -96,5 +111,9 @@ public class GameActivity extends AppCompatActivity {
     public void buttonClicked_settings(View view){
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
+    }
+
+    public void buttonClicked_numberChecker(View view){
+        ImageView imageview = findViewById(R.id.imageButton);
     }
 }
